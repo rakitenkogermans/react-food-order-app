64bb1eac3d058586ec7138b3acbf4ab36f7e28b2
rakitenkogermans <rakitenko.germans@gmail.com>
2021-10-16 13:17:18 +0300

    Cart Context

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 import {useState} from "react";
+import CartProvider from "./context/CartProvider";
 
 function App() {
     const [modalIsShown, setModalIsShown] = useState(false);
@@ -10,13 +11,13 @@ function App() {
     const hideModalHandler = () => setModalIsShown(false);
 
     return (
-        <>
+        <CartProvider>
             {modalIsShown && <Cart onCloseModal={hideModalHandler}/>}
             <Header onShowModal={showModalHandler}/>
             <main>
                 <Meals/>
             </main>
-        </>
+        </CartProvider>
     );
 }
 
